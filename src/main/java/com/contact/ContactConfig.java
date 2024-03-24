28922fbf766b7c7104f70e30af92b53ac2d567e8
Peter David <Princewill016@gmail.com>
2024-03-24 14:12:37 +0100

    bug fixes

--- src/main/java/com/contact/ContactConfig.java
+++ src/main/java/com/contact/ContactConfig.java
@@ -10,29 +10,29 @@
 // @Configuration
 // public class ContactConfig {
 
-//     @SuppressWarnings("null")
+// @SuppressWarnings("null")
 
-//     @Bean
-//     CommandLineRunner commandLineRunner(ContactRepository contactRepository) {
-//         return args -> {
-//             ContactDetails maurizia = new ContactDetails(
+// @Bean
+// CommandLineRunner commandLineRunner(ContactRepository contactRepository) {
+// return args -> {
+// ContactDetails maurizia = new ContactDetails(
 
-//                     "Maurizia Ingry",
-//                     486797233458L,
-//                     "mingry0@paypal.com",
-//                     "32528 Pankratz Plaza");
-//             ContactDetails david = new ContactDetails(
-//                     "David Peter",
-//                     1111111111L,
-//                     "mdbjwwkdkekwjw0@paypal.com",
-//                     "32528 Pankratz ghwggw");
-//             ContactDetails gift = new ContactDetails(
-//                     "Gift Peter",
-//                     412211458L,
-//                     "gift2Y@YAHOO.COM",
-//                     "Waec Estate");
+// "Maurizia Ingry",
+// 486797233458L,
+// "mingry0@paypal.com",
+// "32528 Pankratz Plaza");
+// ContactDetails david = new ContactDetails(
+// "David Peter",
+// 1111111111L,
+// "mdbjwwkdkekwjw0@paypal.com",
+// "32528 Pankratz ghwggw");
+// ContactDetails gift = new ContactDetails(
+// "Gift Peter",
+// 412211458L,
+// "gift2Y@YAHOO.COM",
+// "Waec Estate");
 
-//             contactRepository.saveAll(List.of(maurizia, david, gift));
-//         };
-//     }
+// contactRepository.saveAll(List.of(maurizia, david, gift));
+// };
+// }
 // }
