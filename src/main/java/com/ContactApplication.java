f22f403608ad53e920e18d2e72755484efd2ff5b
Peter David <Princewill016@gmail.com>
2024-03-24 14:09:59 +0100

    Swagger added for api documentataion

--- src/main/java/com/ContactApplication.java
+++ src/main/java/com/ContactApplication.java
@@ -1,12 +1,13 @@
 package com;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ContactApplication {
 
+
+@SpringBootApplication
+
+public class ContactApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ContactApplication.class, args);
 	}
--- src/main/java/com/contact/ContactController.java
+++ src/main/java/com/contact/ContactController.java
@@ -11,10 +11,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
 import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping(path = "api/v1/contact")
+
 
 public class ContactController {
     @Autowired
