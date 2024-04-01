ca08575c08a2b4cab7185d7eacd38493c96fc6fc
Peter David <Princewill016@gmail.com>
2024-04-01 06:03:35 +0100

    minor corrections

--- src/main/java/com/SwaggerConfiguration/SwaggerConfig.java
+++ src/main/java/com/SwaggerConfiguration/SwaggerConfig.java
@@ -6,8 +6,8 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(info = @Info(contact = @Contact(name = "Peter David", email = "Princewill016@gmail.com", url = "princewill016@gmail.com"), description = "This Api Documentation doecuments for Contact Management", title = "Contact Management System", version = "1.0", license = @License(name = "Peter David", url = "https://princewill016@gmail.com"), termsOfService = "Free for All"), servers = {
-        @Server(description = "test env", url = "https://princewill016@gmail.com")
+@OpenAPIDefinition(info = @Info(contact = @Contact(name = "Peter David", email = "Princewill016@gmail.com", url = "princewill016@gmail.com"), description = "This is an Api Documentation  for Contact Management", title = "Contact Management System", version = "1.0", license = @License(name = "Peter David", url = "https://princewill016@gmail.com"), termsOfService = "Free for All"), servers = {
+                @Server(description = "test env", url = "https://princewill016@gmail.com")
 }
 
 )
