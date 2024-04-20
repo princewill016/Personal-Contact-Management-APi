7070ca4f27ed10b7172bd7f9ac2638b2ce4c36a8
Peter David <Princewill016@gmail.com>
2024-04-20 20:42:34 +0100

    bug fixes

---
+++ src/main/java/com/SwaggerConfiguration/ApiDocConfig.java
@@ -0,0 +1,36 @@
+package com.SwaggerConfiguration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class ApiDocConfig {
+    private static final String SECURITY_SCHEME_NAME = "Bearer Token";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("contacts")
+                                .description("contacts")
+                                .version("1.0.0")
+                                .license(
+                                        new License()
+                                                .name("Apache 2.0")))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
